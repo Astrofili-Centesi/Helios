85d14a51229fb91cf82c7424a9ec1ce7f98a0130
Matteo Fortini <matteo.fortini@gmail.com>
2022-05-21 09:02:59 +0200

    Feat: add last whole day

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -3,6 +3,7 @@ import sys
 
 src=sys.argv[1]
 dst=sys.argv[2]
+dst2=sys.argv[3]
 
 d = pd.read_csv(src)
 
@@ -13,3 +14,14 @@ d.drop(columns=['data'],inplace=True)
 d24=d.last('24h')
 
 d24.to_json(dst)
+
+
+# Salva un file con l'ultimo giorno intero
+lastTime=d.index[-1]
+yesterdayFrom=lastTime.floor('1D')-pd.Timedelta('1D')
+yesterdayTo=lastTime.ceil('1D')-pd.Timedelta('1D')
+
+dYesterday=d[(d.index>yesterdayFrom) & (d.index < yesterdayTo)]
+
+dYesterday.to_json(dst2)
+
